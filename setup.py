153f436c5d725af2d7b7deb7fafcf76ba756b674
Rian Hunter <rian@alum.mit.edu>
2018-10-15 14:32:59 -0700

    Release 1.0.6

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('README') as readme:
 
 setup(
     name = 'fusepyng',
-    version='1.0.5',
+    version='1.0.6',
     description = 'Simple ctypes bindings for FUSE',
     long_description = documentation,
     author = 'Giorgos Verigakis',
